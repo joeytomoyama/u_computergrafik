5fb6d33cc0b8efadf70448f41dd502b3a5ebbacc
Joey Prüssing <j.pruessing28@gmail.com>
2023-12-18 03:39:52 +0100

    cleaned up OnePixel class.

--- src/cgtools/OnePixel.java
+++ src/cgtools/OnePixel.java
@@ -6,22 +6,20 @@ public record OnePixel(Sampler sampler, int sampleRate, int x, int y) implements
 
     @Override
     public ColorData call() {
-        // return new ColorData(sampler.getColor(x, y), (int)x, (int)y);
-        // return sampler.getColor(x, y);
+		// supersampling
         Color colorSum = Vector.black;
         for (int xi = 0; xi < sampleRate; xi++) {
             for (int yi = 0; yi < sampleRate; yi++) {
                 double randX = Random.random();
                 double randY = Random.random();
-                // colorSum = sampler.getColor(x + (xi + randX) / sampleRate, y + (yi + randY) / sampleRate);
 				Color sampleColor = sampler.getColor(x + (xi + randX) / sampleRate, y + (yi + randY) / sampleRate);
 				colorSum = Vector.add(colorSum, sampleColor);
             }
         }
 
+		// return corrected color + position
 		int effSamplingRate = sampleRate * sampleRate;
         return new ColorData(
-            // new Color(colorSum.r() / effSamplingRate, colorSum.g() / effSamplingRate, colorSum.b() / effSamplingRate),
             Vector.divide(colorSum, effSamplingRate),
             (int) x,
             (int) y
